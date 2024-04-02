3e11a101361f778fdcc99f974df9998ff21b0eee
Vitalii Yatskiv <vitok2misze@gmail.com>
2024-04-02 15:27:49 +0300

    Added Guest and Recipe Controllers

---
+++ EventPlannerWeb/Controllers/GuestController.cs
@@ -0,0 +1,77 @@
+﻿using EventPlannerWeb.Data;
+using EventPlannerWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventPlannerWeb.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GuestController : Controller
+    {
+
+        private readonly EventPlannerContext _context;
+
+        public GuestController(EventPlannerContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Guest>>> GetGuests()
+        {
+            return await _context.Guest.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Guest>> GetGuest(int id)
+        {
+            var guest = await _context.Guest.FirstOrDefaultAsync(g => g.GuestId == id);
+
+            if (guest == default) return NotFound();
+
+            return guest;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddGuest(Guest guest)
+        {
+            if (ModelState.IsValid)
+            {
+                await _context.Guest.AddAsync(guest);
+                await _context.SaveChangesAsync();
+
+                return Ok();
+            }
+            var message = GetModelValidationErrors();
+
+            return BadRequest(message);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteGuest(int id)
+        {
+            var guest = await _context.Guest.FirstOrDefaultAsync(g => g.GuestId == id);
+            if (guest == default) return NotFound();
+
+            _context.Guest.Remove(guest);
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private IEnumerable<string> GetModelValidationErrors()
+        {
+            return ModelState.Values.SelectMany(v => v.Errors)
+                .Select(v => v.ErrorMessage);
+        }
+    }
+}
+
---
+++ EventPlannerWeb/Controllers/RecipeController.cs
@@ -0,0 +1,77 @@
+﻿using EventPlannerWeb.Data;
+using EventPlannerWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventPlannerWeb.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RecipeController : Controller
+    {
+
+        private readonly EventPlannerContext _context;
+
+        public RecipeController(EventPlannerContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Recipe>>> GetRecipes()
+        {
+            return await _context.Recipe.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Recipe>> GetRecipe(int id)
+        {
+            var recipe = await _context.Recipe.FirstOrDefaultAsync(r => r.RecipeId == id);
+
+            if (recipe == default) return NotFound();
+
+            return recipe;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddRecipe(Recipe recipe)
+        {
+            if (ModelState.IsValid)
+            {
+                await _context.Recipe.AddAsync(recipe);
+                await _context.SaveChangesAsync();
+
+                return Ok();
+            }
+            var message = GetModelValidationErrors();
+
+            return BadRequest(message);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteRecipe(int id)
+        {
+            var recipe = await _context.Recipe.FirstOrDefaultAsync(r => r.RecipeId == id);
+            if (recipe == default) return NotFound();
+
+            _context.Recipe.Remove(recipe);
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private IEnumerable<string> GetModelValidationErrors()
+        {
+            return ModelState.Values.SelectMany(v => v.Errors)
+                .Select(v => v.ErrorMessage);
+        }
+    }
+}
+
