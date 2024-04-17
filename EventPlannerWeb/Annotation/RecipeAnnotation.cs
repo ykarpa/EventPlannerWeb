a23b2b285da4eb160d1e4654d6374bdb204f1bb5
Bozhena Salnikova <bozena.salnikova@gmail.com>
2024-04-17 09:44:43 +0300

    fix some problems with put in views

--- EventPlannerWeb/Annotation/RecipeAnnotation.cs
+++ EventPlannerWeb/Annotation/RecipeAnnotation.cs
@@ -17,7 +17,7 @@
             this.ModelBuilder.Property(u => u.Name).IsRequired().HasMaxLength(20).HasColumnName("name");
             this.ModelBuilder.Property(u => u.Calories).IsRequired().HasColumnName("calories");
             this.ModelBuilder.Property(u => u.CookingTime).IsRequired().HasColumnName("cooking_time");
-            this.ModelBuilder.Property(u => u.CreatedDate).IsRequired().HasColumnName("created_date");
+            this.ModelBuilder.Property(u => u.CreatedDate).IsRequired(false).HasColumnName("created_date");
             this.ModelBuilder.Property(u => u.ModifiedDate).HasColumnName("modified_date");
             this.ModelBuilder.Property(u => u.Description).IsRequired(false).HasColumnName("description");
             this.ModelBuilder.Property(u => u.RecipeImageName).HasColumnName("recipe_image").IsRequired(false);
